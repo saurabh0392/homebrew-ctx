9347ec74d1170a7a433e34f2bc3b6608661bcc1f
Ribbiku <chikkupikku@Ribbikus-MacBook-Pro.local>
2026-08-06 19:57:48 -0400

    chore: mark formula MIT

--- Formula/ctx.rb
+++ Formula/ctx.rb
@@ -4,6 +4,7 @@ class Ctx < Formula
   url "https://github.com/saurabh0392/ctx/releases/download/v0.7.0/ctx-aarch64-apple-darwin.tar.gz"
   sha256 "506138552c539c27f982cdd093c7e1c63716209022c36ad01847a16726282d50"
   version "0.7.0"
+  license "MIT"
 
   depends_on :macos
   depends_on arch: :arm64
